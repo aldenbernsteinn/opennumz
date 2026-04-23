ffde63935d2b2928612ee89672f9b61f66f300ee
Aldenb <dev.aldenb@gmail.com>
2026-04-22 20:36:39 -0700

    Send button: white circle with up arrow

--- open_webui/static/numz-gui.js
+++ open_webui/static/numz-gui.js
@@ -50,7 +50,7 @@
       inputBar.appendChild(thinkBtn);
 
       var sendBtn = el('button', { className: 'numz-send-btn' });
-      sendBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>';
+      sendBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="19" x2="12" y2="5"/><polyline points="5 12 12 5 19 12"/></svg>';
       sendBtn.addEventListener('click', sendMessage);
       inputBar.appendChild(sendBtn);
 
